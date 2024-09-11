d6a4ca07bde5c58a9c6920d84a7da26d722bcf79
prerits02 <146699846+prerits02@users.noreply.github.com>
2024-09-11 22:08:36 +0530

    today's work
    setup the project

---
+++ passop/src/componet/nav.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const nav=()=>{
+    return(
+        <>
+         <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            
+         </nav>
+        </>
+    )
+}
